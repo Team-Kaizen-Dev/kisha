48e1e647980fef8a86714be1b1b653c717c426d5
Michael Ryan Paclibar <michael@satellite.com.ph>
2019-10-19 11:47:25 +0800

    init server

---
+++ kisha-server/src/main/java/com/teamkaizen/kisha/KishaApplication.java
@@ -0,0 +1,13 @@
+package com.teamkaizen.kisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KishaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KishaApplication.class, args);
+    }
+
+}
---
+++ kisha-server/src/test/java/com/teamkaizen/kisha/KishaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamkaizen.kisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KishaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
