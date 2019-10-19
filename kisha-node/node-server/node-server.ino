56d7e30fcc4e4ff75326482073e07fcee1695080
Nakajou Ichinose <unknownymous.user@gmail.com>
2019-10-20 01:03:02 +0800

    Defined a protocol for data transmission and reception

--- kisha-node/node-server/node-server.ino
+++ kisha-node/node-server/node-server.ino
@@ -1,54 +1,39 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
+#define LED 10
+
 RH_RF95 rf95;
 
-int led = 10;
-int counter = 0;
-
-void setup() 
-{
-  pinMode(led, OUTPUT);     
+void setup() {
+  pinMode(LED, OUTPUT);     
   Serial.begin(9600);
-  while (!Serial) ; // Wait for serial port to be available
-  if (!rf95.init())
-    Serial.println("init failed");  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  while (!Serial);
+  if (!rf95.init()) {
+    Serial.println("Initialization failed.");  
+    return;
+  }
   rf95.setFrequency(915);
   rf95.setTxPower(20, false);
-  Serial.print("Max message length: ");
-  Serial.println(RH_RF95_MAX_MESSAGE_LEN);
 }
 
-void loop()
-{
-  if (rf95.available())
-  {
-    // Should be a message for us now   
+void loop() {
+  if (rf95.available()) {
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-    if (rf95.recv(buf, &len))
-    {
-      counter++;
-      Serial.print("data #");
-      Serial.println(counter);
-      digitalWrite(led, HIGH);
-      RH_RF95::printBuffer("request: ", buf, len);
-      Serial.print("got request: ");
+    if (rf95.recv(buf, &len)) {       
+      digitalWrite(LED, HIGH); 
+      Serial.print("DATALOG: ");
       Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
       
       // Send a reply
-      uint8_t data[] = "And hello back to you";
+      uint8_t data[] = "OK";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
-      Serial.println("Sent a reply");
-       digitalWrite(led, LOW);
+       digitalWrite(LED, LOW);
     }
-    else
-    {
-      Serial.println("recv failed");
+    else {
+      Serial.println("ERROR: failed to receive data.");
     }
   }
 }
