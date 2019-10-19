1f00e5aadd5c1bfca867d38101aa9232fa9fae00
Michael Ryan Paclibar <michael@satellite.com.ph>
2019-10-20 01:45:54 +0800

    Added missed JPA Auditing config and added time logged field when saving DataLog

--- kisha-server/src/main/java/com/teamkaizen/kisha/KishaApplication.java
+++ kisha-server/src/main/java/com/teamkaizen/kisha/KishaApplication.java
@@ -3,11 +3,13 @@ package com.teamkaizen.kisha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KishaApplication {
 
     public static void main(String[] args) {
